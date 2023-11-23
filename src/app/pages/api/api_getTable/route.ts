5ac3bfe60000cafa94a62e3335d1ca0bc91daa12
Carlos Silva <guto5543@gmail.com>
2023-11-23 14:02:54 -0300

    modificação

--- src/app/pages/api/api_getTable/route.ts
+++ src/app/pages/api/api_getTable/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server"
 import prisma from "../base"
 
 export async function GET() {
-    const allUniverse = await prisma.$queryRaw`SELECT table_name FROM information_schema.tables WHERE table_catalog = 'multiverse_api' AND table_schema = 'public'`
+    const allUniverse = await prisma.$queryRaw`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`
     return NextResponse.json(allUniverse)
 }
